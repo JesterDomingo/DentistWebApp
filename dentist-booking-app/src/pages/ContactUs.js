feedbf04456194c4c695474da99b631e319eb37e
adelacruz25 <140025987+adelacruz25@users.noreply.github.com>
2023-12-02 23:34:14 -0500

    ContactUs/Service/Updated

--- dentist-booking-app/src/pages/ContactUs.js
+++ dentist-booking-app/src/pages/ContactUs.js
@@ -1,13 +1,38 @@
+// ContactUs.js
 import React from 'react';
-import Header from "../components/header";
+import Header from '../components/header';
+import Footer from '../components/footer';
+import '../csspages/ContactUs.css'; // Create a new CSS file for styles
 
+const ContactUs = () => {
+  return (
+    <div className="contact-us-container">
+      <Header />
+      <div className="content-box">
+        <h2 className="section-title">Contact Us</h2>
+        <p>
+          Have questions or need assistance? Feel free to contact us using the form below, and we'll get back to you as soon as possible.
+        </p>
 
+        {/* Contact Form */}
+        <div className="contact-form">
+          <form>
+            <label htmlFor="name">Name:</label>
+            <input type="text" id="name" name="name" required />
 
-export default function ContactUs(){
-    return (
-        <>
-        <Header />
-        <h2>ContactUs Page</h2>
-        </>
-    )
-}
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" required />
+
+            <label htmlFor="message">Message:</label>
+            <textarea id="message" name="message" rows="4" required></textarea>
+
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default ContactUs;
--- dentist-booking-app/src/pages/Services.js
+++ dentist-booking-app/src/pages/Services.js
@@ -1,13 +1,45 @@
 import React from 'react';
-import Header from "../components/header";
+import Header from '../components/header';
+import Footer from '../components/footer';
+import '../csspages/Services.css';
 
+const Services = () => {
+  return (
+    <div className="services-container">
+      <Header />
+      <div className="content-box">
+        <h2 className="section-title">Our Services</h2>
+        <p>
+          At Smile Reborn Dental Clinic, we offer a comprehensive range of dental services to meet your oral health needs. Our experienced team of dental professionals is dedicated to providing high-quality and personalized care. Explore our services below:
+        </p>
 
+        <div className="service-item">
+          <h3>Dental Check-ups and Cleanings</h3>
+          <p>Regular check-ups and cleanings are essential for maintaining good oral health. Our team will assess your dental health and provide thorough cleanings to keep your smile bright.</p>
+        </div>
 
-export default function Services(){
-    return (
-        <>
-        <Header />
-        <h2>Services Page</h2>
-        </>
-    )
-}
+        <div className="service-item">
+          <h3>Teeth Whitening</h3>
+          <p>Enhance the brightness of your smile with our professional teeth whitening services. Achieve a whiter and more radiant smile in a safe and controlled environment.</p>
+        </div>
+
+        <div className="service-item">
+          <h3>Restorative Dentistry</h3>
+          <p>Restore and strengthen damaged or missing teeth with our restorative dentistry options, including dental crowns, bridges, and implants.</p>
+        </div>
+
+        <div className="service-item">
+          <h3>Orthodontic Treatments</h3>
+          <p>Straighten your teeth and correct bite issues with our orthodontic treatments, including traditional braces and clear aligners.</p>
+        </div>
+
+        {/* Add more service items as needed */}
+      </div>
+
+      {/* Additional content if needed */}
+      <Footer />
+    </div>
+  );
+};
+
+export default Services;
